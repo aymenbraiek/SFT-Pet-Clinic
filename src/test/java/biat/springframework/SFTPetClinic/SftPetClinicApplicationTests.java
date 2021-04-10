0ad8db059d6d01683a4d91dd74f60887b547765b
aymenbraiek <aymen.braiek@esprit.tn>
2021-04-10 19:50:54 +0100

    Initial project Add -closes #1

---
+++ src/test/java/biat/springframework/SFTPetClinic/SftPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package biat.springframework.SFTPetClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SftPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
